d4cb39d0ecf7c1a81272a27f6ad20142b6e44afe
jaaeger <iwan.egerev@yandex.ru>
2022-04-23 18:50:08 +0300

    v_0_3_1

--- main.py
+++ main.py
@@ -98,6 +98,19 @@ class Container(BoxLayout):
                 self.add_widget(self.box_results)
         Container.beacon = x
 
+    def start_tournament(self):
+        self.box_tournament.remove_widget(self.start_t)
+        Container.tournament_list.append(self.id_title.text)
+        Container.tournament_list.append(self.id_date.text)
+        Container.tournament_list.append(self.id_place.text)
+        tournament_box = GridLayout(cols=3, row_force_default=True, row_default_height=35)
+        tournament_box.add_widget(Label(text=Container.tournament_list[0]))
+        tournament_box.add_widget(Label(text=Container.tournament_list[1]))
+        tournament_box.add_widget(Label(text=Container.tournament_list[2]))
+        self.scroll_draw.remove_widget(self.start_d)
+        self.scroll_draw.add_widget(tournament_box)
+        self.scroll_draw.add_widget(self.start_d)
+
     def add_player_scroll(self):
         player = GridLayout(cols=3)
         player.add_widget(Label(text=self.team.text))
@@ -111,13 +124,43 @@ class Container(BoxLayout):
         self.rate.text = ''
         self.country.text = ''
 
-    def add_player_draw(self):
+
+    """def add_player_draw(self):
         i = 0
         player = GridLayout(cols=3)
         while i != len(Container.pl):
             player.add_widget(Label(text=Container.pl[i]))
             i += 1
-        self.scroll_draw.add_widget(player)
+        self.scroll_draw.add_widget(player)"""
+
+    def start_draw(self):
+        # self.scroll_draw.remove_widget(self.show_players)
+        self.scroll_players.remove_widget(self.add_player)
+        self.scroll_draw.remove_widget(self.start_d)
+        continue_d = Button(text='Продолжить жеребьёвку', on_press=lambda x: Container.continue_draw(self))
+        self.scroll_draw.add_widget(continue_d)
+        draw = BoxLayout(orientation='vertical')
+        # draw.add_widget(Button(text='Начать жеребьёвку'))
+        draw.add_widget(Label(text='Пары(0/2)'))
+        tournament_box = GridLayout(cols=5, row_force_default=True, row_default_height=35)
+        tournament_box.add_widget(Label(text=str(Container.pl[0])))
+        tournament_box.add_widget(TextInput())
+        tournament_box.add_widget(Label(text='1:1'))
+        tournament_box.add_widget(TextInput())
+        tournament_box.add_widget(Label(text=str(Container.pl[3])))
+        tournament_box.add_widget(Label(text=str(Container.pl[6])))
+        tournament_box.add_widget(TextInput())
+        tournament_box.add_widget(Label(text='1:1'))
+        tournament_box.add_widget(TextInput())
+        tournament_box.add_widget(Label(text=str(Container.pl[9])))
+        draw.add_widget(tournament_box)
+        # self.remove_widget(self.box_draw)
+        self.scroll_draw.add_widget(draw)
+        child = self.scroll_draw.children
+        child = child[0].children
+        child = child[0].children
+        # print(len(children))
+        print(child)
 
     def continue_draw(self):
         # self.scroll_draw.remove_widget(Button)
@@ -146,44 +189,6 @@ class Container(BoxLayout):
         scroll.add_widget(mid)
         self.box_results.remove_widget(self.result_label)
         self.box_results.add_widget(scroll)
-
-    def start_draw(self):
-        # self.scroll_draw.remove_widget(self.show_players)
-        self.scroll_players.remove_widget(self.add_player)
-        self.scroll_draw.remove_widget(self.start_d)
-        continue_d = Button(text='Продолжить жеребьёвку', on_press=lambda x: Container.continue_draw(self))
-        self.scroll_draw.add_widget(continue_d)
-        draw = BoxLayout(orientation='vertical')
-        # draw.add_widget(Button(text='Начать жеребьёвку'))
-        draw.add_widget(Label(text='Пары(0/2)'))
-        tournament_box = GridLayout(cols=5, row_force_default=True, row_default_height=35)
-        tournament_box.add_widget(Label(text=str(Container.pl[0])))
-        tournament_box.add_widget(TextInput())
-        tournament_box.add_widget(Label(text='1:1'))
-        tournament_box.add_widget(TextInput())
-        tournament_box.add_widget(Label(text=str(Container.pl[3])))
-        tournament_box.add_widget(Label(text=str(Container.pl[6])))
-        tournament_box.add_widget(TextInput())
-        tournament_box.add_widget(Label(text='1:1'))
-        tournament_box.add_widget(TextInput())
-        tournament_box.add_widget(Label(text=str(Container.pl[9])))
-        draw.add_widget(tournament_box)
-        # self.remove_widget(self.box_draw)
-        self.scroll_draw.add_widget(draw)
-
-    def start_tournament(self):
-        self.box_tournament.remove_widget(self.start_t)
-        Container.tournament_list.append(self.id_title.text)
-        Container.tournament_list.append(self.id_date.text)
-        Container.tournament_list.append(self.id_place.text)
-        tournament_box = GridLayout(cols=3, row_force_default=True, row_default_height=35)
-        tournament_box.add_widget(Label(text=Container.tournament_list[0]))
-        tournament_box.add_widget(Label(text=Container.tournament_list[1]))
-        tournament_box.add_widget(Label(text=Container.tournament_list[2]))
-        self.scroll_draw.remove_widget(self.start_d)
-        self.scroll_draw.add_widget(tournament_box)
-        self.scroll_draw.add_widget(self.start_d)
-
 
 
 class TournamentApp(App):
